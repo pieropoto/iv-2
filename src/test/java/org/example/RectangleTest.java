980312e580506e685b4cb8384c4b3d4602bfacd1
pieropoto <pierinox00@gmail.com>
2023-04-17 14:59:16 +0200

    update

--- src/test/java/org/example/RectangleTest.java
+++ src/test/java/org/example/RectangleTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RectangleTest {
+class RectangleTest {
 
     private Rectangle rectangle;
 
